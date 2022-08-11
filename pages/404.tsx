b2d0fc730ad115acd4a99ca7aa0fa7b40631a044
jinnniu <jinnniu23@gmail.com>
2022-08-11 19:04:49 +0900

    edited the sentence

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,5 @@
 import Loader from "../components/Loader";
 
 export default function Notfound() {
-  return <Loader text="There's not the article page" />;
+  return <Loader text="There's no article page" />;
 }
